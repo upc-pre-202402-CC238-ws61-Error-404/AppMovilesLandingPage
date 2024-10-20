00a2972c194085a5a1868c13791554fc0b744fc6
PaoloGuilen <u202124343@upc.edu.pe>
2024-10-19 20:41:07 -0500

    feat: responsive landing page has been made

--- script.js
+++ script.js
@@ -13,3 +13,53 @@ function showTestimonial(n) {
     }
     slides[slideIndex - 1].classList.add("active");
 }
+
+function toggleMenu() {
+    var dropdownMenu = document.getElementById("dropdown-menu");
+    if (dropdownMenu.style.display === "flex") {
+        dropdownMenu.style.display = "none";
+    } else {
+        dropdownMenu.style.display = "flex";
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const imageItems = document.querySelectorAll('#about-product .image-item');
+    let currentIndex = 0;
+    let carouselInterval;
+    let isCarouselActive = false;
+
+    // Iniciar o detener el carrusel dependiendo del tamaño de la pantalla
+    function startCarousel() {
+        const isMobile = window.innerWidth <= 768;
+
+        if (isMobile && !isCarouselActive) {
+            // Iniciar el carrusel solo si no está activo
+            isCarouselActive = true;
+
+            // Ocultar todas las imágenes excepto la primera
+            imageItems.forEach(item => item.style.display = 'none');
+            imageItems[0].style.display = 'block';
+
+            // Cambiar entre las imágenes cada 3 segundos
+            carouselInterval = setInterval(() => {
+                imageItems[currentIndex].style.display = 'none';
+                currentIndex = (currentIndex + 1) % imageItems.length;
+                imageItems[currentIndex].style.display = 'block';
+            }, 3000);
+        } else if (!isMobile && isCarouselActive) {
+            // Detener el carrusel cuando ya no es móvil
+            isCarouselActive = false;
+            clearInterval(carouselInterval);
+
+            // Mostrar todas las imágenes
+            imageItems.forEach(item => item.style.display = 'block');
+        }
+    }
+
+    // Inicializar el comportamiento cuando se carga la página
+    startCarousel();
+
+    // Detectar cambios en el tamaño de la ventana
+    window.addEventListener('resize', startCarousel);
+});
